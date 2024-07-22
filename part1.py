77273b3626ae9957ed55420a3b6c9c26787229ab
irinaalmaeva <almaeva.irina@gmail.com>
2024-07-22 21:57:42 +0300

    add part1

---
+++ part1.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+rows = soup.find_all("tr")
+data = []
+
+
+for row in rows:
+    cols = row.find_all("td")
+    cleaned_cols = [col.text.strip() for col in cols] # очистка от лишних пробелов
+    data.append(cleaned_cols)
+
+    print(data)
+
+
---
+++ part2.py
@@ -0,0 +1,13 @@
+data = [
+
+    ['100', '200', '300'],
+    ['400', '500', '600'],
+  ]
+
+nambers =[]
+
+  for row in data:
+      for text in row:
+          nambers.append(text)
+
+  print(nambers)
