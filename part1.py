ce6c33da2dc5438a31fceca0d56bae26f9871aa0
irinaalmaeva <almaeva.irina@gmail.com>
2024-07-23 00:43:04 +0300

    Update part1.py

--- part1.py
+++ part1.py
@@ -18,9 +18,21 @@ vacancies = driver.find_elements(By.CLASS_NAME, 'vacancy-card--z_UXteNo7bRGzxWVc
 parsed_data = []
 for vacancy in vacancies:
     try:
-        title = vacancy.find_element(By.CLASS_SELECTOR, 'span.vacancy-name--c1Lay3KouCl7XasYakLk ')
-        company = vacancy.find_element(By.CLASS_SELECTOR, 'spain.company-info-text--vgvZouLtf8jwBmaD1xgp')
-        salary = vacancy.find_element(By.CLASS_SELECTOR, 'spain.compensation-text--kTJ0_rp54B2vNeZ3CTt2')
-        link = vacancy.find_element(By.CLASS_SELECTOR, 'serp-item__title').get_attribute('href')
+        title = vacancy.find_element(By.CSS_SELECTOR, 'span.vacancy-name--c1Lay3KouCl7XasYakLk').text
+        company = vacancy.find_element(By.CSS_SELECTOR, 'spain.company-info-text--vgvZouLtf8jwBmaD1xgp').text
+        salary = vacancy.find_element(By.CSS_SELECTOR, 'spain.compensation-text--kTJ0_rp54B2vNeZ3CTt2').text
+        link = vacancy.find_element(By.CSS_SELECTOR, 'a.bloko-link').get_attribute('href')
 
-    parsed_data.append([title, company, link])
+    except Exception as e:
+        print('Произошла ошибка при парсинге данных:', str(e))
+        continue
+
+    parsed_data.append([title, company, salary, link])
+
+    driver.quit()
+    with open ("hh.csv", 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(["Название вакансии", "Компания", "Зарплата", "Ссылка на вакансию"])
+        writer.writerows(parsed_data)
+
+
