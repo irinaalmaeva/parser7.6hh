b207e917404fa8bbbbf6a14eac5a8cebecf62d90
irinaalmaeva <almaeva.irina@gmail.com>
2024-07-24 00:41:01 +0300

    change teg span

--- part1.py
+++ part1.py
@@ -25,9 +25,11 @@ parsed_data = []
 for vacancy in vacancies:
     try:
         title = vacancy.find_element(By.CSS_SELECTOR, 'span.vacancy-name--c1Lay3KouCl7XasYakLk').text
-        company = vacancy.find_element(By.CSS_SELECTOR, 'spain.company-info-text--vgvZouLtf8jwBmaD1xgp').text
-        salary = vacancy.find_element(By.CSS_SELECTOR, 'spain.compensation-text--kTJ0_rp54B2vNeZ3CTt2').text
+        company = vacancy.find_element(By.CSS_SELECTOR, 'span.company-info-text--vgvZouLtf8jwBmaD1xgp').text
+        salary = vacancy.find_element(By.CSS_SELECTOR, 'span.compensation-text--kTJ0_rp54B2vNeZ3CTt2').text
         link = vacancy.find_element(By.CSS_SELECTOR, 'a.bloko-link').get_attribute('href')
+
+
 
     except Exception as e:
         print('Произошла ошибка при парсинге данных:', str(e))
