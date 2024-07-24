0c760ce8e81ec62ef15ac345f303e97cd2ba9d44
irinaalmaeva <almaeva.irina@gmail.com>
2024-07-24 22:12:11 +0300

    add part3,  file hh.csv

--- part3.py
+++ part3.py
@@ -2,8 +2,7 @@ import time
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 driver = webdriver.Chrome()
 
@@ -12,16 +11,12 @@ url = "https://hh.ru/vacancies/finansovyy_menedzher"
 # Запуск браузера(открытие сайта)
 driver.get(url)
 
-wait = WebDriverWait(driver, 10)
+time.sleep(3)
 
-try:
-    vacancies = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'vacancy-card--z_UXteNo7bRGzxWVcL7y')))
-except Exception as e:
-    print("Произошла ошибка при загрузке вакансий:", str(e))
-    driver.quit()
-    exit()
+vacancies = driver.find_elements(By.CLASS_NAME, 'vacancy-card--z_UXteNo7bRGzxWVcL7y')
 
 parsed_data = []
+
 for vacancy in vacancies:
     try:
         title = vacancy.find_element(By.CSS_SELECTOR, 'span.vacancy-name--c1Lay3KouCl7XasYakLk').text
@@ -31,14 +26,15 @@ for vacancy in vacancies:
 
 
 
-    except Exception as e:
-        print('Произошла ошибка при парсинге данных:', str(e))
+    except :
+        print('Произошла ошибка при парсинге данных:')
         continue
 
     parsed_data.append([title, company, salary, link])
 
-    driver.quit()
-    with open ("hh.csv", 'w', newline='', encoding='utf-8') as file:
+driver.quit()
+
+with open("hh.csv", 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerows(["Название вакансии", "Компания", "Зарплата", "Ссылка на вакансию"])
         writer.writerows(parsed_data)
